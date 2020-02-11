1db430e177e6e58b7a481acb475e2d765eb50cfb
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2020-02-11 13:42:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,44 @@
+@@ -1,21 +1,28 @@
 from discord.ext import commands
+import discord
 import os
 import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+client = discord.Client()
 
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content == "/ping":
+        await message.channel.send("pong!")
 
+token = os.environ['DISCORD_BOT_TOKEN']
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.command(pass_context=True)
+async def ping2(msg):  # 処理時間を返す
+    startt = time.time()
+    await msg.send("計測中……!")
+    await msg.send("pong！\n結果:**"+str(round(time.time()-startt, 3))+"**秒だ！")
+
+    @client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content == "/ssrlogin":
+        await message.channel.send("..login")
 
 bot.run(token)
+client.run(token)
